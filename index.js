2c0c10a579ca7fdc187300448236a990d200ea84
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-03-26 13:38:35 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add() {
+    console.log("add")
+}
+function subtract() {
+    console.log("subtract")
+}
+function multiply() {
+    console.log("multiply")
+}
+function divide() {
+    console.log("divide")
+}
+function add(a,b){
+    return a+b
+}
+function subtract(a,b){
+    return a-b
+}
+function multiply(a,b){
+    return a*b
+}
+function divide(a,b){
+    return a/b
+}
+function increment(n){
+    return n+1
+}
+function decrement(n){
+    return n-1
+}
+function makeInt(n){
+    return (parseInt(n,10))
+}
+function makeInt(n){
+    return (parseFloat(n,10))
+}
+function preserveDecimal(n) {
+    return parseFloat(n,10)
+}
+
